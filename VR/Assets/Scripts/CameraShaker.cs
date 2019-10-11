901c6b8110de99fbb150566578db4b5a47769663
MohanadZ <mzeito14@student.aau.dk>
2019-10-11 10:56:31 +0200

    Setup, Avatar, and Shaking

---
+++ VR/Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    public void ShakeCamera(float duration, float xMagnitude, float yMagnitude, float zMagnitude, float frequency)
+    {
+        StartCoroutine(Shake(duration, xMagnitude, yMagnitude, zMagnitude, frequency));
+    }
+
+    IEnumerator Shake(float duration, float xMagnitude, float yMagnitude, float zMagnitude, float frequency)
+    {
+        Quaternion originalRotation = transform.localRotation;
+        float timeElapsed = 0f;
+
+        while(timeElapsed < duration)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(
+            xMagnitude * (Mathf.PerlinNoise(3, Time.time * frequency) * 2 - 1),
+            yMagnitude * (Mathf.PerlinNoise(4, Time.time * frequency) * 2 - 1),
+            zMagnitude * (Mathf.PerlinNoise(5, Time.time * frequency) * 2 - 1)));
+
+            timeElapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localRotation = originalRotation;
+    }
+}
---
+++ VR/Assets/Scripts/Shaker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour
+{
+    [Header("Camera Shake Settings")]
+    [SerializeField] float cameraShakeDuration = 2f;
+    [SerializeField] float cameraShakeFrequency = 25f;
+    [SerializeField] Vector3 maximumAngularShake = Vector3.one * 2;
+
+    [Header("Object Shake Settings")]
+    [SerializeField] float objectShakeDuration = 2f;
+    [SerializeField] float objectShakeFrequency = 25f;
+    [SerializeField] Vector3 maximumTranslationShake = Vector3.one * 0.5f;
+
+    [Header("Gameobjects")]
+    [SerializeField] ObjectShaker shakeableObject;
+    [SerializeField] CameraShaker vrCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shakeableObject.GetComponent<ObjectShaker>();
+        vrCamera.GetComponent<CameraShaker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //vrCamera.ShakeCamera(cameraShakeDuration, maximumAngularShake.x, maximumAngularShake.y, maximumAngularShake.z, cameraShakeFrequency);
+
+            shakeableObject.ShakeObject(objectShakeDuration, maximumTranslationShake.x, maximumTranslationShake.y, maximumTranslationShake.z, objectShakeFrequency);
+        }
+    }
+}
