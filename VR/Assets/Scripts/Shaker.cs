fc3eec67136994797aa61cddca519e331f6b3738
metigel94 <leon-mueller@hotmail.de>
2019-10-13 13:02:21 +0200

    Revert "Audio Generator Function Implemented"
    This reverts commit aa9bb4647e5d8864be359645f190d9e494d4fb2b.

--- VR/Assets/Scripts/Shaker.cs
+++ VR/Assets/Scripts/Shaker.cs
@@ -6,31 +6,23 @@ public class Shaker : MonoBehaviour
 {
     [Header("Camera Shake Settings")]
     [SerializeField] float cameraShakeDuration = 2f;
-    [SerializeField] float cameraShakeFrequency = 100f;
+    [SerializeField] float cameraShakeFrequency = 25f;
     [SerializeField] Vector3 maximumAngularShake = Vector3.one * 2;
 
     [Header("Object Shake Settings")]
     [SerializeField] float objectShakeDuration = 2f;
-    [SerializeField] float objectShakeFrequency = 100f;
+    [SerializeField] float objectShakeFrequency = 25f;
     [SerializeField] Vector3 maximumTranslationShake = Vector3.one * 0.5f;
-
-    [Header("Audio Frequency / Synchronization")]
-
-    [Range(1, 20000)]  //Creates a slider in the inspector
-    [SerializeField] float audioFrequency = 100f;
-    [SerializeField] float audioDuration = 2f;
 
     [Header("Gameobjects")]
     [SerializeField] ObjectShaker shakeableObject;
     [SerializeField] CameraShaker vrCamera;
-    [SerializeField] FrequencySynchronization frequencyGenerator;
 
     // Start is called before the first frame update
     void Start()
     {
         shakeableObject.GetComponent<ObjectShaker>();
         vrCamera.GetComponent<CameraShaker>();
-        frequencyGenerator.GetComponent<FrequencySynchronization>();
     }
 
     // Update is called once per frame
@@ -41,9 +33,6 @@ public class Shaker : MonoBehaviour
             //vrCamera.ShakeCamera(cameraShakeDuration, maximumAngularShake.x, maximumAngularShake.y, maximumAngularShake.z, cameraShakeFrequency);
 
             shakeableObject.ShakeObject(objectShakeDuration, maximumTranslationShake.x, maximumTranslationShake.y, maximumTranslationShake.z, objectShakeFrequency);
-
-            frequencyGenerator.GenerateAudio(audioDuration, audioFrequency);
-
         }
     }
 }
