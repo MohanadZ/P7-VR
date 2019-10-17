5faee6aeeb517794a7290d6207e5c0cc86efdd88
metigel94 <leon-mueller@hotmail.de>
2019-10-17 10:25:25 +0200

    Implemented basic Server-Client configuration

---
+++ Python/Server.py
@@ -0,0 +1,39 @@
+import time
+import zmq
+
+class Connection:
+    string_message = "FROM PYTHON TO UNITY"
+
+    @staticmethod
+    def server():
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind("tcp://*:5555")
+
+        messages = []
+    
+        while True:
+            #  Wait for next request from client
+            message = socket.recv()
+            print("Received request: %s" % message)
+
+            Connection.string_message = message.decode("utf-8")
+
+            
+            
+            error_feedback = Connection.string_message
+            feedbackToUnity = error_feedback.encode()
+
+            #  In the real world usage, you just need to replace time.sleep() with
+            #  whatever work you want python to do.-
+            #time.sleep(1)
+            messages.append(message)
+
+            #  Send reply back to client
+            #  In the real world usage, after you finish your work, send your output here
+            socket.send(feedbackToUnity)
+
+
+# Instantiate Connection class and call server-function.
+server = Connection()
+server.server()
---
+++ VR/Assets/Client/Scripts/HelloClient.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using TMPro;
+
+public class HelloClient : MonoBehaviour
+{
+    private HelloRequester _helloRequester;
+    public bool SendPack = true;
+
+    private void Start()
+    {
+        _helloRequester = new HelloRequester();
+        _helloRequester.Start();
+        _helloRequester.frequency = 200;
+    }
+
+    void Update()
+    {
+        if (SendPack)
+        {
+            _helloRequester.Continue();
+        }
+        else if (!SendPack)
+        {
+            _helloRequester.Pause();
+        }
+    }
+
+
+    private void OnDestroy()
+    {
+        _helloRequester.Stop();
+    }
+}
