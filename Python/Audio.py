2c18d274e0bec414829378787954dc38e98247ed
MohanadZ <mzeito14@student.aau.dk>
2019-11-05 15:00:29 +0100

    Minor change
    Python folder + Generate Lighting

---
+++ Python/Audio.py
@@ -0,0 +1,36 @@
+import pyaudio
+import numpy as np
+
+
+class AudioClass:
+
+    isPlaying = False
+
+    @staticmethod
+    def playback(frequency, duration):
+
+        p = pyaudio.PyAudio()
+
+        volume = 0.5     # range [0.0, 1.0]
+        fs = 44100       # sampling rate, Hz, must be integer
+
+        # generate samples, note conversion to float32 array
+        samples = (np.sin(2*np.pi*np.arange(fs*duration)*frequency/fs)).astype(np.float32)
+
+        # for paFloat32 sample values must be in range [-1.0, 1.0]
+        stream = p.open(format=pyaudio.paFloat32,
+                        channels=2,
+                        rate=fs,
+                        output=True)
+
+        # play. May repeat with different volume values (if done interactively)
+        
+        AudioClass.isPlaying = True
+        stream.write((volume*samples).tobytes())
+        # stream.write(volume*samples)
+
+        stream.stop_stream()
+        stream.close()
+        
+        AudioClass.isPlaying = False
+        p.terminate()
---
+++ Python/Server.py
@@ -0,0 +1,49 @@
+import time
+import zmq
+import timeit
+from Audio import AudioClass
+
+class Connection:
+
+    string_message = ""
+
+    @staticmethod
+    def server():
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind("tcp://*:5555")
+
+        audio = AudioClass
+
+        # messages = []
+    
+        while True:
+
+            #  Wait for next request from client
+            message = socket.recv()
+            # print("Received request: %s" % message + str(audio.isPlaying))
+            Connection.string_message = message.decode("utf-8")
+
+            freq,dur,play = Connection.string_message.split(",")
+
+            if(float(play) == 1 and audio.isPlaying == False):
+                audio.playback(float(freq), float(dur)*2)
+
+
+            # messageToUnity = Connection.string_message
+            # stringToSend = "test"
+            # stringToUnity = stringToSend.encode()
+
+            #  In the real world usage, you just need to replace time.sleep() with
+            #  whatever work you want python to do.-
+            #time.sleep(1)
+            # messages.append(message)
+
+            #  Send reply back to client
+            #  In the real world usage, after you finish your work, send your output here
+            socket.send(b'stringToSend')
+
+
+# Instantiate Connection class and call server-function.
+server = Connection()
+server.server()
