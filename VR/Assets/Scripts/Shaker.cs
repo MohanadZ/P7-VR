ab44779f79ffadd1d0fafdd268b3216af6e91264
MohanadZ <mzeito14@student.aau.dk>
2019-11-05 14:37:26 +0100

    Merged branches

---
+++ VR/Assets/Scripts/Shaker.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour
+{
+    [Header("Server Settings")]
+    [SerializeField] float serverDelay = 0.3f;
+
+    [Header("Camera Shake Settings")]
+    [SerializeField] float cameraShakeDuration = 2f;
+    [SerializeField] float cameraShakeFrequency = 25f;
+    [SerializeField] Vector3 maximumAngularShake = Vector3.one * 2;
+
+    public float CameraShakeDuration
+    {
+        get
+        {
+            return cameraShakeDuration;
+        }
+        set
+        {
+            cameraShakeDuration = value;
+        }
+    }
+
+    public float CameraShakeFrequency
+    {
+        get
+        {
+            return cameraShakeFrequency;
+        }
+        set
+        {
+            cameraShakeFrequency = value;
+        }
+    }
+
+    [Header("Object Shake Settings")]
+    [SerializeField] float objectShakeDuration = 2f;
+    [SerializeField] float objectShakeFrequency = 25f;
+    [SerializeField] Vector3 maximumTranslationShake = Vector3.one * 0.5f;
+
+    public float ObjectShakeDuration
+    {
+        get
+        {
+            return objectShakeDuration;
+        }
+        set
+        {
+            objectShakeDuration = value;
+        }
+    }
+
+    public float ObjectShakeFrequency
+    {
+        get
+        {
+            return objectShakeFrequency;
+        }
+        set
+        {
+            objectShakeFrequency = value;
+        }
+    }
+
+    [Header("Gameobjects")]
+    [SerializeField] ObjectShaker shakeableObject;
+    [SerializeField] CameraShaker vrCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shakeableObject.GetComponent<ObjectShaker>();
+        vrCamera.GetComponent<CameraShaker>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            vrCamera.ShakeCamera(cameraShakeDuration, serverDelay, maximumAngularShake.x, maximumAngularShake.y, maximumAngularShake.z, cameraShakeFrequency);
+
+            shakeableObject.ShakeObject(objectShakeDuration, serverDelay, maximumTranslationShake.x, maximumTranslationShake.y, maximumTranslationShake.z, objectShakeFrequency);
+
+        }
+    }
+}
