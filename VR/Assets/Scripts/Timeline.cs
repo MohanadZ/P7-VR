3781eb5acfe7f9bb790243da40740f52f1f72076
Dinklebergg <32732509+Dinklebergg@users.noreply.github.com>
2019-11-10 13:59:36 +0100

    Audio and Signals

--- VR/Assets/Scripts/Timeline.cs
+++ VR/Assets/Scripts/Timeline.cs
@@ -15,7 +15,10 @@ public class Timeline : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartTimeline();
+        }
     }
 
     public void StartTimeline()
