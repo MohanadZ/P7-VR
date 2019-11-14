be3454ba7fa758354798e48c19c5297e74563871
MohanadZ <mzeito14@student.aau.dk>
2019-11-14 16:02:35 +0100

    Fixed hand position
    + attempt to make a laser pointer

--- VR/Assets/Scripts/ScalableHeight.cs
+++ VR/Assets/Scripts/ScalableHeight.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class ScalableHeight : MonoBehaviour
 {
     [SerializeField] float initialHeight = 0.7f;
+    [SerializeField] GameObject rightHand, leftHand;
     [SerializeField] Camera camera;
-    [SerializeField] float cameraSpeed = 2.0f;
+    //[SerializeField] float cameraSpeed = 2.0f;
 
     float currentHeight, scaleFactor;
 
@@ -25,24 +27,30 @@ public class ScalableHeight : MonoBehaviour
         //Debug.Log("Scale is " + scaleFactor);
 
         transform.localScale = Vector3.one * scaleFactor;
+        rightHand.transform.localScale = Vector3.one * scaleFactor;
+        leftHand.transform.localScale = Vector3.one * scaleFactor;
+
+        Debug.Log("Scale Factor is " + scaleFactor);
+        Debug.Log("Right Hand is " + rightHand.transform.localScale);
+        Debug.Log("Camera position is " + transform.localScale);
     }
 
-    private void PlayerAdjustHeight()
-    {
-        float height = 0.0f;
+    //private void PlayerAdjustHeight()
+    //{
+    //    float height = 0.0f;
 
-        //var playerControl = Input.GetAxis("Player Height");
-        //print("..... " + playerControl);
+    //    //var playerControl = Input.GetAxis("Player Height");
+    //    //print("..... " + playerControl);
 
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            height += Vector3.up.y;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            height -= Vector3.up.y;
-        }
+    //    if (Input.GetKey(KeyCode.UpArrow))
+    //    {
+    //        height += Vector3.up.y;
+    //    }
+    //    if (Input.GetKey(KeyCode.DownArrow))
+    //    {
+    //        height -= Vector3.up.y;
+    //    }
 
-        transform.position += new Vector3(0, height * cameraSpeed * Time.deltaTime, 0);
-    }
+    //    transform.position += new Vector3(0, height * cameraSpeed * Time.deltaTime, 0);
+    //}
 }
